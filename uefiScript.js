d20be6588b340be8e2101e24356e6a353efd54cb
RoMan-robots <romkozhan@gmail.com>
2024-05-08 12:10:49 +0300

    rlly

--- uefiScript.js
+++ uefiScript.js
@@ -2,5 +2,9 @@ const uefiButton = document.getElementsByClassName("uefi-button");
 
 function runFromUEFI(){
     const bootSelect = document.querySelector(".select-boot").value;
-    const bootChoise = bootSelect.value;
+    if(bootSelect == "usb"){
+        console.log(bootSelect)
+    } else {
+        
+    }
 }
