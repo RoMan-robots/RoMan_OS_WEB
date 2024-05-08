c5b7be58c9e2e8ad761f58e2deb3d218e670e6ef
RoMan-robots <romkozhan@gmail.com>
2024-05-08 12:06:41 +0300

    upgraded uefi

---
+++ uefiScript.js
@@ -0,0 +1,6 @@
+const uefiButton = document.getElementsByClassName("uefi-button");
+
+function runFromUEFI(){
+    const bootSelect = document.querySelector(".select-boot").value;
+    const bootChoise = bootSelect.value;
+}
