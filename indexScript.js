5eabeed446e4a106fd8fc46da96c4d0ee596e392
RoMan-robots <romkozhan@gmail.com>
2024-05-14 17:40:57 +0300

    oobe done

--- indexScript.js
+++ indexScript.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded',()=>{
   const reason = new URLSearchParams(window.location.search).get('reason');
-  console.log(reason)
     setTimeout(function() {
         if(reason == "oobe") {
           window.location.href = '/oobe.html'
         } else if(reason == "reboot"){
-          
+          window.location.href = '/desktop.html'
         } else {
           window.location.href = '/uefi.html'
         }
-      }, 3500);
+      }, 4000);
 })
--- oobeScript.js
+++ oobeScript.js
@@ -1,2 +1,45 @@
-const oobeMusic = new Audio('./oobeMusic.mp3')
+const oobeMusic = new Audio('./oobeMusic.mp3');
 oobeMusic.play();
+
+function nextStep(step) {
+    if (step == 2) {
+        let WiFiChoise = document.getElementById("wi-fi-select").value;
+        if (WiFiChoise == "wi-fi-premium") {
+            alert("WiFi Error: ця мережа не може бути застосована на вашому комп'ютері, відсутній Premium Chip X. Будь ласка, підключіться до іншої мережі");
+            return;
+        } else if (WiFiChoise == "wi-fi-virus") {
+            document.querySelector(`.oobe${step}`).style.display = "none";
+            document.querySelector(`.wi-fi-error-screen`).style.display = "block";
+            return;
+        } else if(WiFiChoise == "wi-fi-free"){
+
+        } else {
+            alert("WiFi Error: Ваш комп'ютер не підтримує серверні мережи. Будь ласка, підключіться до іншого WiFi");
+            return;
+        }
+    }else if(step >= 3 && step < 7){
+        const allName = document.getElementById('all-name').value;
+        const username = document.getElementById('username').value;
+        if(!allName || !username){
+            alert("Будь ласка, заповніть повне ім'я та ім'я адміністратора");
+            return;
+        }
+        setInterval(()=>{
+            nextStep(step +1)
+        }, 3500);
+    }
+    document.querySelector(`.oobe${step}`).style.display = "none";
+    document.querySelector(`.oobe${step + 1}`).style.display = "block";
+}
+
+function skipWiFi() {
+    document.getElementById("wi-fi-select").value = "wi-fi-free";
+    nextStep(2)
+}
+
+function fatalReboot() {
+    window.location.href = "/";
+}
+function reboot() {
+    window.location.href = "/?reason=reboot";
+}
