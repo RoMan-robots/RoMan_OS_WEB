5506624057ba3b1724d1b31d642a5e9813ce5f22
RoMan-robots <romkozhan@gmail.com>
2024-06-27 12:27:47 +0300

    testing failed

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require("url")
 
-function createWindow () {
-  const mainWindow = new BrowserWindow({
+let mainWindow;
+
+async function createWindow() {
+  mainWindow = new BrowserWindow({
     width: 800,
-    height: 600
-  })
+    height: 600,
+    webPreferences: {
+      webSecurity: false,
+    }
+  });
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, "index.html"),
+    protocol: "file",
+    slashes: true
+  }));
 
-  mainWindow.loadFile('index.html')
+  mainWindow.webContents.openDevTools()
+
+  ses.on('will-redirect', (event, oldUrl, newUrl, isMainFrame, httpResponseCode, httpStatusText) => {
+    console.log(`Redirecting from ${oldUrl} to ${newUrl}`);
+    if (newUrl === '/uefi.html') {
+      mainWindow.loadFile(path.join(__dirname, 'uefi.html'));
+    }
+  });
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  })
 }
 
-app.on('ready', createWindow)
+app.on("ready", createWindow)
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+app.on("window-all-closed", () => {
+  app.quit()
 })
